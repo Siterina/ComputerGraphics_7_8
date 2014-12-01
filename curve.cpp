5e2582055164cd0cb9da41d13a5096ae8dd5f593
Siterina <ritsu-nee@yandex.ru>
2014-12-01 21:25:32 +0300

    maybe final

--- curve.cpp
+++ curve.cpp
@@ -8,14 +8,14 @@
 Curve::Curve(QWidget *parent) :
     QWidget(parent),
 
-    drawCurve(false),
-    style("SolidLine"),
     colour("black"),
     penWidth(1),segments(1),
     h(height()), w(width()),
     cycle(false), alpha(0),
     max_x(0), max_y(0),
     min_x(10000), min_y(10000),
+    tensionPoint(0), tensionValueX(1), tensionValueY(1),
+    mChange(false),
 
     ui(new Ui::Curve)
 {
@@ -27,17 +27,13 @@ Curve::~Curve()
     delete ui;
 }
 
-//void changeStyle(QString style, QPen pen) {
-
-//    if(style == "SolidLine")
-//        pen.setStyle(Qt::SolidLine);
-//    if(style == "DashLine")
-//        pen.setStyle(Qt::DashLine);
-//    if(style == "DotLine")
-//        pen.setStyle(Qt::DotLine);
-//    if(style == "DashDotLine")
-//        pen.setStyle(Qt::DashDotLine);
-//}
+QPointF mabs(QPointF t) {
+    if(t.x() < 0)
+        t.setX(t.x() * (-1));
+    if(t.y() < 0)
+        t.setY(t.y() * (-1));
+    return t;
+}
 
 
 QPointF Curve::splain(double x, QPointF Pk, QPointF Pk1, QPointF Mk, QPointF Mk1) {
@@ -49,16 +45,11 @@ QPointF Curve::splain(double x, QPointF Pk, QPointF Pk1, QPointF Mk, QPointF Mk1
             (-2*t*t*t + 3*t*t) * Pk1 +
             (t*t*t - t*t) * (Pk1.x() - Pk.x()) * Mk1;
 
-    return result;
+    return mabs(result);
 }
 
 
-
 void Curve::paintEvent(QPaintEvent*) {
-
-   // double Ky = h / height();
-    //double Kx = w / width();
-    //double K = std::min(Kx, Ky);
 
     QPainter painter(this);
     painter.setPen(QPen(Qt::black, 5));
@@ -73,22 +64,13 @@ void Curve::paintEvent(QPaintEvent*) {
 
 
     QPen penCurve;
-    //changeStyle(style, penCurve);
     penCurve.setWidth(penWidth);
     penCurve.setColor(colour);
     painter.setPen(penCurve);
     if(curves.size()) {
         for(int i = 0; i < curves.size(); i++) {
-            if(cycle)
+            if(cycle && curves[i].size() > 2)
                 curves[i].push_back(curves[i][0]);
-           /* for(int j = 0; j < curves[i].size(); j++) { // draw points
-                curves[i][j].setX(curves[i][j].x() - max_x / 2.0);
-                curves[i][j].setY(curves[i][j].y() - max_y / 2.0);
-                curves[i][j] = toRotate * curves[i][j];
-                curves[i][j].setX(curves[i][j].x() + max_x / 2.0);
-                curves[i][j].setY(curves[i][j].y() + max_y/ 2.0);
-                // qDebug() << toRotate.data[0][0];
-            }*/
 
 
             for(int j = 0; j <= curves[i].size() - 1; j++) { // count differential vector
@@ -100,52 +82,54 @@ void Curve::paintEvent(QPaintEvent*) {
                     m.push_back((curves[i][j+1] - curves[i][j-1]) / (curves[i][j+1].x() - curves[i][j-1].x()));
 
             }
-            for(int j = 0; j < curves[i].size() - 1; j++) { // draw curve
+
+            qDebug() << mChange << " " << m.size() << " " << tensionPoint << endl;
+
+            if(mChange) {
+                m[tensionPoint].setX(tensionValueX);
+                m[tensionPoint].setY(tensionValueY);
+            }
+
+            for(int j = 0; j < curves[i].size() - 1; j++) { // form vector to draw curve
                 double step = 0.05;
-                QPointF prev, next;
-                prev.setX(curves[i][j].x());
-                prev.setY(curves[i][j].y());
-               // toDraw.push_back(curves[i][j]);
                 if(curves[i][j].x() < curves[i][j+1].x())
                     for(double x = curves[i][j].x(); x < curves[i][j+1].x() + step; x+=step) {
                         toDraw.push_back(splain(x, curves[i][j], curves[i][j+1], m[j], m[j+1]));
-                        //next = splain(x, curves[i][j], curves[i][j+1], m[j], m[j+1]);
-                        //painter.drawLine(prev, next);
-                        //prev = next;
                     }
-                else {
+                else
                     for(double x = curves[i][j].x(); x > curves[i][j+1].x() - step; x-=step) {
                         toDraw.push_back(splain(x, curves[i][j], curves[i][j+1], m[j], m[j+1]));
-                        //next = splain(x, curves[i][j], curves[i][j+1], m[j], m[j+1]);
-                        //painter.drawLine(prev, next);
-                        //prev = next;
                     }  
-                }
-
-                for(int i = 0; i < toDraw.size(); i++) {
-                    toDraw[i].setX(toDraw[i].x() - (max_x + min_x)/ 2.0);
-                    toDraw[i].setY(toDraw[i].y() - (max_y + min_y)/ 2.0);
-                    toDraw[i] = toRotate * toDraw[i];
-                    toDraw[i].setX(toDraw[i].x() + (max_x + min_x)/ 2.0);
-                    toDraw[i].setY(toDraw[i].y() + (max_y + min_y)/ 2.0);
-                }
-
-                for(int i = 0; i < toDraw.size() - 1; i++) {
-                    painter.drawLine(toDraw[i], toDraw[i+1]);
-                }
 
             }
+
+             //qDebug() << toDraw.size() << endl;
+             qDebug() << m[0] << endl;
+             qDebug() << m[1] << endl;
+
+
+            for(int k = 0; k < toDraw.size(); k++) { // rotate curve
+                toDraw[k].setX(toDraw[k].x() - (max_x + min_x)/ 2.0);
+                toDraw[k].setY(toDraw[k].y() - (max_y + min_y)/ 2.0);
+                toDraw[k] = toRotate * toDraw[k];
+                toDraw[k].setX(toDraw[k].x() + (max_x + min_x)/ 2.0);
+                toDraw[k].setY(toDraw[k].y() + (max_y + min_y)/ 2.0);
+            }
+
+            for(int k = 0; k < toDraw.size() - 1; k++) { // draw curve
+                painter.drawLine(toDraw[k], toDraw[k+1]);
+            }
+
+
             toDraw.clear();
             m.clear();
-            if(cycle)
+            if(cycle && curves[i].size() > 2)
                 curves[i].pop_back();
         }
     }
 
 
 } // end of paintEvent
-
-
 
 
 /***********************************************/
@@ -173,7 +157,6 @@ void Curve::mousePressEvent(QMouseEvent *e) {
 }
 
 
-
 void Curve::on_exit_clicked() {
     close();
 }
@@ -182,11 +165,6 @@ void Curve::on_colour_change_currentTextChanged(const QString &arg1) {
     colour = arg1;
     repaint();
 }
-
-//void Curve::on_style_change_currentTextChanged(const QString &arg1) {
-//   style = arg1;
-//   repaint();
-//}
 
 void Curve::on_segments_valueChanged(int arg1) {
     segments = arg1;
@@ -230,4 +208,29 @@ void Curve::on_cycleBox_toggled(bool checked) {
 void Curve::on_angleChange_valueChanged(int value) {
     alpha = (double)value * pi / 180;
     repaint();
+}
+
+void Curve::on_pointToSetTension_valueChanged(int arg1) {
+    if(curves[0].size() > arg1 - 1) {
+        tensionPoint = arg1 - 1;
+        mChange = true;
+        repaint();
+    }
+    else mChange = false;
+}
+
+void Curve::on_setTensionX_valueChanged(double arg1) {
+    tensionValueX = arg1;
+}
+
+void Curve::on_setTensionY_valueChanged(double arg1) {
+    tensionValueY = arg1;
+}
+
+void Curve::on_changeTension_clicked() {
+        if(curves[0].size() > tensionPoint) {
+            mChange = true;
+            repaint();
+        }
+        else mChange = false;
 }
--- curve.h
+++ curve.h
@@ -23,18 +23,21 @@ public:
     ~Curve();
 
     QString colour;
-    QString style;
     int penWidth;
     int segments;
     QStack <QVector <QPointF> > curves;
     QVector <QPointF> temp;
     QVector <QPointF> toDraw;
     QVector <QPointF> m;
-    bool drawCurve;
     int h, w;
     bool cycle;
     double alpha;
     int max_x, max_y, min_x, min_y;
+
+    int tensionPoint;
+    double tensionValueX;
+    double tensionValueY;
+    bool mChange;
 
 private:
     Ui::Curve *ui;
@@ -46,7 +49,6 @@ protected:
 private slots:
     void on_exit_clicked();
     void on_colour_change_currentTextChanged(const QString &arg1);
-    //void on_style_change_currentTextChanged(const QString &arg1);
     void on_segments_valueChanged(int arg1);
     void on_width_valueChanged(int arg1);
     void on_add_curve_clicked();
@@ -54,6 +56,10 @@ private slots:
     void on_delete_all_clicked();
     void on_cycleBox_toggled(bool checked);
     void on_angleChange_valueChanged(int value);
+    void on_pointToSetTension_valueChanged(int arg1);
+    void on_setTensionX_valueChanged(double arg1);
+    void on_setTensionY_valueChanged(double arg1);
+    void on_changeTension_clicked();
 };
 
 #endif // CURVE_H
